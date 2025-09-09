1d7598a529faf5897f06e09e377a9a78ddabf101
Rafael Lumertz Webber <rafael.webber09@gmail.com>
2025-09-08 21:17:05 -0300

    Commit inicial com estrutura do projeto

---
+++ exercicioAula007/src/main/java/com/example/exercicioAula007/ExercicioAula007Application.java
@@ -0,0 +1,13 @@
+package com.example.exercicioAula007;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioAula007Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioAula007Application.class, args);
+	}
+
+}
---
+++ exercicioAula007/src/test/java/com/example/exercicioAula007/ExercicioAula007ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercicioAula007;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioAula007ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
